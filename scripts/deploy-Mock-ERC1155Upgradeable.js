48bd34f55c61fdb31b4bacc738dd06a92aaf52ee
Marc-Aurele Besner <82244926+marc-aurele-besner@users.noreply.github.com>
2022-05-13 07:41:50 -0400

    prettier scripts/

--- scripts/deploy-Mock-ERC1155Upgradeable.js
+++ scripts/deploy-Mock-ERC1155Upgradeable.js
@@ -7,7 +7,12 @@ async function main() {
     const mockERC1155Upgradeable = await MockERC1155Upgradeable.deploy();
 
     await mockERC1155Upgradeable.deployed();
-    await hre.addressBook.saveContract('MockERC1155Upgradeable', mockERC1155Upgradeable.address, hre.network.name, deployer.address);
+    await hre.addressBook.saveContract(
+        'MockERC1155Upgradeable',
+        mockERC1155Upgradeable.address,
+        hre.network.name,
+        deployer.address
+    );
     await mockERC1155Upgradeable.initialize('MockERC1155Upgradeable', 'MOCK', 'https://google.com');
 
     console.log('MockERC1155Upgradeable deployed to:', mockERC1155Upgradeable.address);
@@ -16,6 +21,6 @@ async function main() {
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+        console.error(error);
+        process.exit(1);
+    });
--- scripts/deploy-Mock-ERC20Upgradeable.js
+++ scripts/deploy-Mock-ERC20Upgradeable.js
@@ -7,7 +7,12 @@ async function main() {
     const mockERC20Upgradeable = await MockERC20Upgradeable.deploy();
 
     await mockERC20Upgradeable.deployed();
-    await hre.addressBook.saveContract('MockERC20Upgradeable', mockERC20Upgradeable.address, hre.network.name, deployer.address);
+    await hre.addressBook.saveContract(
+        'MockERC20Upgradeable',
+        mockERC20Upgradeable.address,
+        hre.network.name,
+        deployer.address
+    );
     await mockERC20Upgradeable.initialize('MockERC20Upgradeable', 'MOCK');
 
     console.log('MockERC20Upgradeable deployed to:', mockERC20Upgradeable.address);
@@ -16,6 +21,6 @@ async function main() {
 main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error(error)
-        process.exit(1)
-    })
+        console.error(error);
+        process.exit(1);
+    });
